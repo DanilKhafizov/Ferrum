9839c0b89c2d8696b025258999cbf8d9c11c99f7
Danil Khafizov <danilkhafizov123kk@gmail.com>
2023-09-07 23:16:43 +0500

    Changes 22:03 (add trainer's images and bad activity_main.xml

--- app/src/main/java/com/khafizov/ferrum/MainActivity.java
+++ app/src/main/java/com/khafizov/ferrum/MainActivity.java
@@ -1,14 +1,49 @@
 package com.khafizov.ferrum;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private int [] images = {R.drawable.pavel, R.drawable.jason, R.drawable.sarah};
 
+    private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(new ImagePagerAdapter(getSupportFragmentManager()));
     }
-}
+
+        private class ImagePagerAdapter extends FragmentPagerAdapter {
+            public ImagePagerAdapter(FragmentManager fm) {
+                super(fm);
+            }
+
+
+
+            @NonNull
+            @Override
+            public Fragment getItem(int position) {
+                FragmentImage fragment = new FragmentImage();
+                Bundle bundle = new Bundle();
+                int imagesposition = 0;
+                bundle.putInt("image", imagesposition);
+                fragment.setArguments(bundle);
+                return fragment;
+            }
+
+            @Override
+            public int getCount() {
+                return images.length;
+            }
+        }
+    }
+
